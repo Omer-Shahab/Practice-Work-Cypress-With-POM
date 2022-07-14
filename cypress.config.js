652109d3f5df70d02e403b7e46fe82cdcfe0f3c7
CULTUx2@38 <syedmuhammadomershahab@gmail.com>
2022-07-14 14:01:53 +0500

    Cypress End-to-End with POM

---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  watchForFileChanges: false,
+  username: 'admin',
+  password: 'password',
+  reporter: 'cypress-multi-reporters',
+  reporterOptions: {
+    reporterEnabled: 'mochawesome',
+    mochawesomeReporterOptions: {
+      reportDir: 'cypress/reports/mocha',
+      quite: true,
+      overwrite: false,
+      html: false,
+      json: true,
+    },
+  },
+  e2e: {
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config)
+    },
+    baseUrl: 'http://eaapp.somee.com',
+  },
+}
